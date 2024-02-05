db3e20bbf6da8983d0e31575ee15029cc6c13cde
JoseAlfonsoVivo <josealfonsovivo@gmail.com>
2024-02-05 18:52:32 +0100

    he creado una nueva rama

--- dart1/main.dart
+++ dart1/main.dart
@@ -1,6 +1,9 @@
-void main() {
-
-print('Hola Mundo');
-
-
+void main(List<String> arguments) {
+    // Obtener la fecha y hora actual
+    DateTime now = DateTime.now();
+    // Formatear la fecha y hora
+    String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
+    // Imprimir el mensaje con la fecha y hora
+    var dart_application_1;
+    print('Hello world: ${dart_application_1.calculate()}! ($formattedDateTime)');
 }
